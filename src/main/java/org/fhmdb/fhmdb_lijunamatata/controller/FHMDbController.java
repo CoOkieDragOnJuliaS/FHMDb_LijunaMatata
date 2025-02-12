b212912333e2a8e222dcd32c76f7a71294b55e96
01304814 <e1304814@student.tuwien.ac.at>
2025-02-12 23:30:55 +0100

    Exercise1 [Lilie] call initializeMovies in Controller

--- src/main/java/org/fhmdb/fhmdb_lijunamatata/controller/FHMDbController.java
+++ src/main/java/org/fhmdb/fhmdb_lijunamatata/controller/FHMDbController.java
@@ -3,8 +3,10 @@ package org.fhmdb.fhmdb_lijunamatata.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import org.fhmdb.fhmdb_lijunamatata.Movie;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class FHMDbController {
@@ -12,10 +14,11 @@ public class FHMDbController {
         private Label welcomeText;
         --> annotation for a element on the GUI
     */
-
+    private List<Movie> movies;
 
     public FHMDbController() {
         //No args constructor for initialization
+        movies = Movie.initializeMovies();
     }
 
     //TODO: Sort Button!
