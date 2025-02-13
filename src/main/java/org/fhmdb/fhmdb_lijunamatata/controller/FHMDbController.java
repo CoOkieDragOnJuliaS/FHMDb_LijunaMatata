c2b8aca482d5e876bb9771bc28632616af157b5d
CoOkieDragOnJuliaS <julia.sass@stud.fh-campuswien.ac.at>
2025-02-13 12:36:01 +0100

    Exercise1 [Julia] Redesign of the cells, measurements of the cells with help of a fellow student

--- src/main/java/org/fhmdb/fhmdb_lijunamatata/controller/FHMDbController.java
+++ src/main/java/org/fhmdb/fhmdb_lijunamatata/controller/FHMDbController.java
@@ -1,19 +1,32 @@
 package org.fhmdb.fhmdb_lijunamatata.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 import org.fhmdb.fhmdb_lijunamatata.models.Movie;
+import org.fhmdb.fhmdb_lijunamatata.ui.MovieCell;
 
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class FHMDbController {
-   /*   @FXML
-        private Label welcomeText;
-        --> annotation for a element on the GUI
-    */
+public class FHMDbController implements Initializable {
+
     private List<Movie> movies;
+    @FXML
+    ListView<Movie> movieListView;
 
     public FHMDbController() {
         //No args constructor for initialization
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
         movies = Movie.initializeMovies();
+        movieListView.setItems(FXCollections.observableArrayList(movies));
+        movieListView.setCellFactory(movieListView -> new MovieCell());
     }
 
     //TODO: Sort Button!
---
+++ src/main/java/org/fhmdb/fhmdb_lijunamatata/ui/MovieCell.java
@@ -0,0 +1,61 @@
+package org.fhmdb.fhmdb_lijunamatata.ui;
+
+import org.fhmdb.fhmdb_lijunamatata.models.Movie;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+/**
+ * @author Julia Sass
+ * @date 13.02.2025
+ * This class is the design cell for the Movie object inside the UI
+ */
+public class MovieCell extends ListCell<Movie> {
+    private final Label title = new Label();
+    private final Label description = new Label();
+    private final Label genre = new Label();
+    private final VBox layout = new VBox(title, description, genre);
+
+    @Override
+    protected void updateItem(Movie movie, boolean empty) {
+        super.updateItem(movie, empty);
+
+        if (empty || movie == null) {
+            setText(null);  //sets the text of all labels inside the item null
+        } else {
+            this.getStyleClass().add("movie-cell");
+            title.setText(movie.getTitle());
+            description.setText(
+                    movie.getDescription() != null
+                            ? movie.getDescription()
+                            : "No description available"
+            );
+            genre.setText(String.join(", ", movie.getGenres().stream()
+                    .map(Enum::name)
+                    .toArray(String[]::new)));
+
+
+            // color scheme
+            title.getStyleClass().add("text-yellow");
+            title.getStyleClass().add("start-title");
+            description.getStyleClass().add("text-white");
+            description.getStyleClass().add("start-description");
+            genre.getStyleClass().add("text-white");
+            genre.getStyleClass().add("start-genre");
+            layout.setBackground(new Background(new BackgroundFill(Color.web("#454545"), null, null)));
+
+            // layout
+            description.setMaxWidth(this.getScene().getWidth() - 30);
+            description.setWrapText(true);
+
+            layout.setPadding(new Insets(10));
+            layout.spacingProperty().set(10);
+            layout.alignmentProperty().set(javafx.geometry.Pos.CENTER_LEFT);
+            setGraphic(layout);
+        }
+    }
+}
