5bf33206bd5bc0eeac271dff18737dcb6c7af326
MaratDavletshin <davletshinm@list.ru>
2025-02-14 16:27:24 +0100

    connected FHMDbController with fxml Buttons filterBtn, genreComboBox, sortBtn, searchField

--- src/main/java/org/fhmdb/fhmdb_lijunamatata/controller/FHMDbController.java
+++ src/main/java/org/fhmdb/fhmdb_lijunamatata/controller/FHMDbController.java
@@ -1,19 +1,28 @@
 package org.fhmdb.fhmdb_lijunamatata.controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import org.fhmdb.fhmdb_lijunamatata.models.Movie;
 
 import java.util.List;
 
 public class FHMDbController {
-   /*   @FXML
-        private Label welcomeText;
-        --> annotation for a element on the GUI
-    */
+
+    public Button filterBtn;
+    public ComboBox genreComboBox;
+    public Button sortBtn;
+    public TextField searchField;
+    /*   @FXML
+            private Label welcomeText;
+            --> annotation for a element on the GUI
+        */
     private List<Movie> movies;
 
     public FHMDbController() {
         //No args constructor for initialization
         movies = Movie.initializeMovies();
+
     }
 
     //TODO: Sort Button!
@@ -21,6 +30,7 @@ public class FHMDbController {
     //TODO: Filtering with query
 
     //TODO: Filtering with genres
+    
 
     //TODO: Updating Logic
     //Working with elements from the view has to have the annotation @FXML, please look up JavaFX if you are uncertain
