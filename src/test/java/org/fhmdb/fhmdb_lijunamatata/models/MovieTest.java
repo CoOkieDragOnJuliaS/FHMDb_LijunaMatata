76331ebe8f5da0634c7f30e4087083485dd9e345
01304814 <e1304814@student.tuwien.ac.at>
2025-03-05 15:48:26 +0100

    Exercise1 [Lilie] test: add paramterized tests in MovieTest

--- src/test/java/org/fhmdb/fhmdb_lijunamatata/models/MovieTest.java
+++ src/test/java/org/fhmdb/fhmdb_lijunamatata/models/MovieTest.java
@@ -5,7 +5,10 @@ import org.fhmdb.fhmdb_lijunamatata.models.Movie;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,6 +35,15 @@ public class MovieTest {
         assertEquals(expectedTitle, this.movies.get(0).getTitle());
     }
 
+    @DisplayName("Test initialize Movies: All Movies have expected title")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4})
+    public void testInitializeMovies_allMovieTitles_equal_expected(int index) {
+        List<String> expectedTitles = new ArrayList<>(List.of("Life Is Beautiful", "The Usual Suspects", "Puss in " +
+                "Boots", "Avatar", "The Wolf of Wall Street"));
+        assertEquals(expectedTitles.get(index), this.movies.get(index).getTitle());
+    }
+
     @Test
     @DisplayName("Test initialize Movies: First Movie has expected description")
     public void testInitializeMovies_firstMovieDescription_equals_expected() {
@@ -41,6 +53,29 @@ public class MovieTest {
         assertEquals(expectedDescription, this.movies.get(0).getDescription());
     }
 
+    @DisplayName("Test initialize Movies: All Movies have expected description")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4})
+    public void testInitializeMovies_allMovieDescription_equal_expected(int index) {
+        List<String> expectedDescriptions = new ArrayList<>(List.of("When an open-minded Jewish librarian and his son" +
+                        " " +
+                        "become victims of" +
+                " the Holocaust, he uses a perfect mixture of will, humor, and imagination to protect his son from " +
+                "the dangers around their camp."
+                , "A sole survivor tells of the twisty events " +
+                "leading up to a horrific gun battle on a boat, which begin when five criminals meet at a seemingly " +
+                "random police lineup."
+                , "An outlaw cat, his childhood egg-friend, and a seductive thief kitty " +
+                "set out in search for the eggs of the fabled Golden Goose to clear his name, restore his lost honor," +
+                " and regain the trust of his mother and town."
+                , "A paraplegic Marine dispatched to the moon Pandora on a unique mission " +
+                "becomes torn between following his orders and protecting the world he feels is his home."
+                , "Based on the true story of Jordan Belfort, from his rise to " +
+                "a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal " +
+                "government."));
+        assertEquals(expectedDescriptions.get(index), this.movies.get(index).getDescription());
+    }
+
     @Test
     @DisplayName("Test initialize Movies: First Movie has expected genres")
     public void testInitializeMovies_firstMovieGenres_equals_expected() {
@@ -48,4 +83,16 @@ public class MovieTest {
         assertEquals(expectedGenres, this.movies.get(0).getGenres());
     }
 
+    @DisplayName("Test initialize Movies: All Movies have expected genre")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4})
+    public void testInitializeMovies_allMovieGenres_equal_expected(int index) {
+        List<List<Genre>> expectedGenreLists = new ArrayList<>(List.of(
+                List.of(Genre.DRAMA, Genre.ROMANCE),
+                List.of(Genre.CRIME, Genre.DRAMA, Genre.MYSTERY),
+                List.of(Genre.COMEDY, Genre.FAMILY, Genre.ANIMATION),
+                List.of(Genre.ANIMATION, Genre.DRAMA, Genre.ACTION),
+                List.of(Genre.DRAMA, Genre.ROMANCE, Genre.BIOGRAPHY)));
+        assertEquals(expectedGenreLists.get(index), this.movies.get(index).getGenres());
+    }
 }
