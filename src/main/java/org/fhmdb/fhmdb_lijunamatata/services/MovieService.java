1a7b7f0641a65dd1acd5e76d70959d1c7cdd7da7
CoOkieDragOnJuliaS <julia.sass@stud.fh-campuswien.ac.at>
2025-03-31 20:32:01 +0200

    Exercise2 [Julia] feature: implemented the streaming methods

--- src/main/java/org/fhmdb/fhmdb_lijunamatata/services/MovieService.java
+++ src/main/java/org/fhmdb/fhmdb_lijunamatata/services/MovieService.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Provides utility methods for sorting and filtering movies.
@@ -53,6 +55,7 @@ public class MovieService {
     }
 
     //TODO: Discuss if this method is still needed if we have the fetchfilteredMovies with API
+    //This is still the method for the testing base in MovieService Test --> test data is important
     /**
      * Filters a list of movies based on the provided search text and selected genre.
      * The method checks if the movie's title contains the search text (ignoring case)
@@ -101,20 +104,77 @@ public class MovieService {
                 movie.getDescription().toLowerCase().contains(searchText.toLowerCase());
     }
 
-    //TODO: Java Streams methods & TESTING (TDD)
+    /**
+     * Streams through a list of movies to get the actor who appears most frequently in the main cast
+     *
+     * @param movies
+     * @return the most popular actor as String
+     */
     public String getMostPopularActor(List<Movie> movies) {
-        return "";
+        /*
+        short version:
+        return movies.stream()
+                .flatMap(movie -> movie.getMainCast().stream())
+                .collect(Collectors.groupingBy(actor -> actor, Collectors.counting()))
+                .entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
+         */
+        //create a flat map of a stream of the list of actors inside the movie
+        //collect it by grouping it by actors and the count of appearance in a new Map<String,Long>
+        Map<String, Long> actorCount =  movies.stream()
+                .flatMap(movie -> movie.getMainCast().stream())
+                .collect(Collectors.groupingBy(actor -> actor, Collectors.counting()));
+
+        //Stream through the new map of actors and their appearance count and get the max values & compare them
+        //map them to get the key value of the Map (which is the actor) and return the value
+        return actorCount.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
     }
 
+    /**
+     * Streams through a list of movies and counts the length of the longest title of the movies
+     *
+     * @param movies
+     * @return the count of the characters used in the longest title
+     */
     public int getLongestMovieTitle(List<Movie> movies) {
-        return 0;
+        //Stream through movies, map to get the title and compare the length with comparingInt()
+        return movies.stream()
+                .map(Movie::getTitle)
+                .max(Comparator.comparingInt(String::length))
+                .get().length();
     }
 
-    public long countMoviesFrom(List<Movie> movies, String director) {
-        return 0;
+    /**
+     * Streams through a list of movies and the String value director to get the movies from the specific director
+     *
+     * @param movies
+     * @param director
+     * @return the count of the movies from the director which is set as a parameter
+     */
+    public long countMoviesFromDirector(List<Movie> movies, String director) {
+        //stream through the movies and filter by looking into the directors list to see if it contains the specific director
+        return movies.stream()
+                .filter(movie -> movie.getDirectors().contains(director))
+                .count();
     }
 
+    /**
+     * Streams through a list of movies and uses the startYear and endYear parameters to get the movies which are between 2 years
+     *
+     * @param movies
+     * @param startYear
+     * @param endYear
+     * @return a list of movies between the specific years of the parameters
+     */
     public List<Movie> getMoviesBetweenYears(List<Movie> movies, int startYear, int endYear) {
-        return null;
+        //stream through movies and filter with releaseYear >= startYear and <= endYear
+        return movies.stream()
+                .filter(movie -> movie.getReleaseYear() >= startYear && movie.getReleaseYear() <= endYear)
+                .collect(Collectors.toList());
     }
 }
