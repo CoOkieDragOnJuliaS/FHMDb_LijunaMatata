fe3e5e0a04bb10c311e36d63584cf6b369051011
Lil <loonaarc@gmail.com>
2025-04-27 12:03:40 +0200

    Exercise3 [Lilie] build: grant access to ./db folder to ormlite.jdbc + add./db to .gitignore

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,7 +8,8 @@ module org.fhmdb.fhmdb_lijunamatata {
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
-    requires java.logging;
+    requires ormlite.jdbc;
+    requires java.sql;
 
     opens org.fhmdb.fhmdb_lijunamatata to javafx.fxml;
     opens org.fhmdb.fhmdb_lijunamatata.controller to javafx.fxml;
@@ -19,5 +20,5 @@ module org.fhmdb.fhmdb_lijunamatata {
     exports org.fhmdb.fhmdb_lijunamatata.models;
     exports org.fhmdb.fhmdb_lijunamatata.api;
     exports org.fhmdb.fhmdb_lijunamatata.database;
-    opens org.fhmdb.fhmdb_lijunamatata.database to com.google.gson, javafx.fxml;
+    opens org.fhmdb.fhmdb_lijunamatata.database to com.google.gson, javafx.fxml, ormlite.jdbc;
 }
